0605da124fcfc4506668dea593bdb7bbfa59a1f7
spicehava <spicehava@126.com>
2018-10-24 18:20:15 +0800

    第一天，github创建项目spring boot

---
+++ src/main/java/com/jfund/JfundsApplication.java
@@ -0,0 +1,12 @@
+package com.jfund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfundsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jfund/JfundsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jfund;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JfundsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
