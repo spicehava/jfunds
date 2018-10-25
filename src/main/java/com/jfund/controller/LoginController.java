2860f6833f6c911d3c307da9b95c22fcd87445a7
spicehava <spicehava@126.com>
2018-10-25 10:06:01 +0800

    第二次提交

---
+++ src/main/java/com/jfund/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.jfund.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+	@RequestMapping("index")
+	public String index() {
+		return "/index.html";
+	}
+}
